08fe286ab194504a9ebd6b83333ddaa3c68641fe
Sho <shoyukimiukanae@gmail.com>
2018-10-15 19:21:31 +0900

    途中経過(10/15 3rd)

--- Podfile
+++ Podfile
@@ -2,4 +2,5 @@ use_frameworks!
 
 target 'WSFA' do
   pod 'IBMWatsonVisualRecognitionV3', '~> 0.35.0'
+  pod 'SVProgressHUD','2.2.5'
 end
--- WSFA/SubViewController.swift
+++ WSFA/SubViewController.swift
@@ -7,8 +7,12 @@
 //
 
 import UIKit
+import SceneKit
+import SVProgressHUD
 
 class SubTableViewController: UITableViewController {
+    
+    @IBOutlet weak var StrageResults: UIBarButtonItem!
     
     // 解析結果はAppDelegateの変数に入っている
     private let appDelegate: AppDelegate = UIApplication.shared.delegate as! AppDelegate
@@ -35,4 +39,35 @@ class SubTableViewController: UITableViewController {
         cell.setData(data: result)
         return cell
     }
+    
+    
+    @IBAction func StrageResults(_ sender: Any) {
+        //コンテキスト開始
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height), false, 0.0)
+        //viewを書き出す
+        self.view.drawHierarchy(in: self.view.bounds, afterScreenUpdates: true)
+        // imageにコンテキストの内容を書き出す
+        let image: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        //コンテキストを閉じる
+        UIGraphicsEndImageContext()
+        // imageをカメラロールに保存
+        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+        
+        SVProgressHUD.showSuccess(withStatus: "フォトライブラリに\n追加されました！")
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        tableView.sectionHeaderHeight = CGFloat(25.0)
+        
+        let headerView = UITableViewHeaderFooterView()
+        headerView.backgroundColor = UIColor.lightGray
+        headerView.textLabel?.text = "顔解析の結果発表〜！！"
+        headerView.textLabel?.textColor = UIColor.white
+        headerView.textLabel?.textAlignment = NSTextAlignment.center
+        
+        return headerView
+    }
+    
+    
 }
